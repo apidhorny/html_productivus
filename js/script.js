da884eac5c4353b77a83b9e16946863b8655156f
Andriy <andriy.pidhorny@gmail.com>
2021-10-25 23:04:23 +0300

    add js: menu-burger and gallery FSLIGHTBOX

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const HEADER = document.querySelector('.section-header');
+
+document.querySelector('.faq-accordion').addEventListener('click', (event) => {
+    if (event.target.closest('.faq-accordion__item')) {
+        event.target.closest('.faq-accordion__item').classList.toggle('faq-accordion__item--active');
+    }
+});
+
+document.querySelector('.btn-burger').addEventListener('click', (event) => {
+    HEADER.classList.toggle('section-header--active-nav');
+});
+
+const resetNav = () => {
+    HEADER.classList.remove('section-header--active-nav');
+    console.log('done');
+};
+
+window.addEventListener('resize', resetNav);
+
+// TODO: make consts
+// TODO: навeсти порядок в коді js
