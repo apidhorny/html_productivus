9d28bd03a26501c9880fe51a9c531a6924727b8e
Andriy <andriy.pidhorny@gmail.com>
2021-10-26 23:08:31 +0300

    add two slider

--- js/script.js
+++ js/script.js
@@ -13,10 +13,53 @@ document.querySelector('.btn-burger').addEventListener('click', (event) => {
 
 const resetNav = () => {
     HEADER.classList.remove('section-header--active-nav');
-    console.log('a');
 };
 
 window.addEventListener('resize', resetNav);
+
+new Swiper('.section-hero-image', {
+    // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
+new Swiper('.slider-blog-container', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.dots',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.btn-blog--next',
+        prevEl: '.btn-blog--prev',
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
 
 // TODO: make consts
 // TODO: навeсти порядок в коді js
