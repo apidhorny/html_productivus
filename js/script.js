2cd52e2d604f02b351531b46dbf496110157e6a4
Andriy <andriy.pidhorny@gmail.com>
2021-10-27 07:11:00 +0300

    push all site

--- js/script.js
+++ js/script.js
@@ -1,65 +1,41 @@
 const HEADER = document.querySelector('.section-header');
-const a = 3;
+const ACCORDION = document.querySelector('.faq-accordion');
+const BTN_BURGER = document.querySelector('.btn-burger');
 
-document.querySelector('.faq-accordion').addEventListener('click', (event) => {
+ACCORDION.addEventListener('click', (event) => {
     if (event.target.closest('.faq-accordion__item')) {
         event.target.closest('.faq-accordion__item').classList.toggle('faq-accordion__item--active');
     }
 });
 
-document.querySelector('.btn-burger').addEventListener('click', (event) => {
+BTN_BURGER.addEventListener('click', () => {
     HEADER.classList.toggle('section-header--active-nav');
 });
 
-const resetNav = () => {
+const resetNavigation = () => {
     HEADER.classList.remove('section-header--active-nav');
 };
 
-window.addEventListener('resize', resetNav);
+window.addEventListener('resize', resetNavigation);
 
+// SLIDER SWIPER
 new Swiper('.section-hero-image', {
-    // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
+    loop: true,
 
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-
-    // Navigation arrows
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
 });
 new Swiper('.slider-blog-container', {
-    // Optional parameters
-    // direction: 'vertical',
     loop: true,
 
-    // If we need pagination
     pagination: {
         el: '.dots',
     },
 
-    // Navigation arrows
     navigation: {
         nextEl: '.btn-blog--next',
         prevEl: '.btn-blog--prev',
     },
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
 });
-
-// TODO: make consts
-// TODO: навeсти порядок в коді js
